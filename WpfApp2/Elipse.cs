de9f37224b7959950545be3f2163c77b36a3e84e
@y.ivanov <bountyman797@gmail.com>
2022-02-06 16:10:39 +0200

    Add project files.

---
+++ WpfApp2/Elipse.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    internal class Elipse
+    {
+        int x1;
+        int y1;
+        int x2;
+        int y2;
+
+        public Elipse(int x1, int y1, int x2, int y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+        public bool isCirle()
+        {
+            return x2 == y2;
+        }
+        public void Draw(Canvas canvas)
+        {
+            Ellipse ellipse = new Ellipse { Width = x2, Height = y2, Fill = Brushes.Green };
+            Canvas.SetTop(ellipse, y1);
+            Canvas.SetLeft(ellipse, x1);
+            canvas.Children.Add(ellipse);
+        }
+        public string Info()
+        {
+            return $"Эллипс с координатами: x1 - {x1}, y1 - {y1}, x2 - {x2}, y2 - {y2}";
+        }
+    }
+}
---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            canva.Children.Clear();
+            Elipse elipse = new Elipse(int.Parse(X1.Text), int.Parse(Y1.Text), int.Parse(X2.Text), int.Parse(Y2.Text));
+            elipse.Draw(canva);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Elipse elipse = new Elipse(int.Parse(X1.Text), int.Parse(Y1.Text), int.Parse(X2.Text), int.Parse(Y2.Text));
+            MessageBox.Show(elipse.isCirle() ? "Да" : "Нет");
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Elipse elipse = new Elipse(int.Parse(X1.Text), int.Parse(Y1.Text), int.Parse(X2.Text), int.Parse(Y2.Text));
+            MessageBox.Show(elipse.Info());
+        }
+    }
+}
